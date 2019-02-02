1720f4afdf78a5ea410889319deae8d13ba106dc
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2019-02-02 18:12:28 +0300

    Added Worker struct for sending code from the ThreadPool to a thread

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::thread;
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
@@ -15,14 +15,14 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
         
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
-            // create some threads and store them in the vector
+        for id in 0..size {
+            workers.push(Worker::new(id));
         }
 
         ThreadPool {
-            threads
+            workers
         }
     }
 
@@ -31,4 +31,20 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static {
             
         }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker {
+            id,
+            thread,
+        }
+    }
 }
