86b8d4edff6afe9b827774835c3406f2cd2a5215
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2019-02-05 18:09:54 +0300

    Finished signaling to the threads to stop listening for jobs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,15 +6,17 @@ use rust_webserver::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        let pool = ThreadPool::new(4);
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
